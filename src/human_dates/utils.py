afde513d4c55c8a2efe84961485cf15b7786a4c4
Alessandro Candido <candido.ale@gmail.com>
2020-10-19 15:58:31 +0200

    Convert bare days difference in a calendar difference

--- src/human_dates/utils.py
+++ src/human_dates/utils.py
@@ -1,23 +1,23 @@
-from datetime import datetime
+from datetime import datetime, date
 import numbers
 
 
 def _parse_time_from_input(time, name=""):
     """
-        parse time representation into a time object
+    parse time representation into a time object
 
-        if time is None return :py:meth:`datetime.now`
+    if time is None return :py:meth:`datetime.now`
 
-        Parameters
-        ----------
-        time : number or datetime
-            the time to be converted (if number it's considered a timestamp, see
-            :py:meth:`datetime.timestamp`)
+    Parameters
+    ----------
+    time : number or datetime
+        the time to be converted (if number it's considered a timestamp, see
+        :py:meth:`datetime.timestamp`)
 
-        Returns
-        -------
-        datetime.datetime
-            the time object created
+    Returns
+    -------
+    datetime.datetime
+        the time object created
     """
     # __import__("pdb").set_trace()
 
@@ -35,23 +35,23 @@ def _parse_time_from_input(time, name=""):
 
 def _get_time_diff(time_beg, time_end=None):
     """
-        get the difference of two objects representing times (as a time)
+    get the difference of two objects representing times (as a time)
 
-        Parameters
-        ----------
-        time_beg : int or datetime
-            the beginning of the time interval (if int it's considered a
-            timestamp, see :py:meth:`datetime.timestamp`), if None use the
-            current time
-        time_end : int or datetime
-            the end of the time interval (if int it's considered a timestamp, see
-            :py:meth:`datetime.timestamp`), if None use the current time
-            (default: None)
+    Parameters
+    ----------
+    time_beg : int or datetime
+        the beginning of the time interval (if int it's considered a
+        timestamp, see :py:meth:`datetime.timestamp`), if None use the
+        current time
+    time_end : int or datetime
+        the end of the time interval (if int it's considered a timestamp, see
+        :py:meth:`datetime.timestamp`), if None use the current time
+        (default: None)
 
-        Returns
-        -------
-        datetime.timedelta
-            the difference as a time interval
+    Returns
+    -------
+    datetime.timedelta
+        the difference as a time interval
 
     """
 
@@ -67,24 +67,54 @@ def _get_time_diff(time_beg, time_end=None):
     return time_end - time_beg
 
 
+def _get_calendar_diff(time_beg, time_end=None):
+    """
+    get the difference of two objects representing times (as number of dates
+    passed)
+
+    Parameters
+    ----------
+    time_beg : int or datetime
+        the beginning of the time interval (if int it's considered a
+        timestamp, see :py:meth:`datetime.timestamp`), if None use the
+        current time
+    time_end : int or datetime
+        the end of the time interval (if int it's considered a timestamp, see
+        :py:meth:`datetime.timestamp`), if None use the current time
+        (default: None)
+
+    Returns
+    -------
+    int
+        the difference as the number of days (with sign)
+
+    """
+
+    time_beg = _parse_time_from_input(time_beg, "time_beg").date()
+
+    time_end = _parse_time_from_input(time_end, "time_end").date()
+
+    return time_end - time_beg
+
+
 def _is_future(time, time_ref=None):
     """
-        check if `time` is in future (w.r.t. `time_ref`, by default it is now)
+    check if `time` is in future (w.r.t. `time_ref`, by default it is now)
 
-        Parameters
-        ----------
-        time : int or datetime
-            the time to check (if int it's considered a
-            timestamp, see :py:meth:`datetime.timestamp`)
-        time_ref : int or datetime
-            the time reference (if int it's considered a timestamp, see
-            :py:meth:`datetime.timestamp`), if None use the present time
-            (default: None)
+    Parameters
+    ----------
+    time : int or datetime
+        the time to check (if int it's considered a
+        timestamp, see :py:meth:`datetime.timestamp`)
+    time_ref : int or datetime
+        the time reference (if int it's considered a timestamp, see
+        :py:meth:`datetime.timestamp`), if None use the present time
+        (default: None)
 
-        Returns
-        -------
-        bool
-            is in future or not
+    Returns
+    -------
+    bool
+        is in future or not
 
     """
 
@@ -100,10 +130,10 @@ def _is_future(time, time_ref=None):
 
 def _is_past(time, time_ref=None):
     """
-        check if `time` is in past (w.r.t. `time_ref`, by default it is now)
+    check if `time` is in past (w.r.t. `time_ref`, by default it is now)
 
-        provided for convenience, is the opposite of :py:meth:`_is_future` by
-        implementation (so same time means past, by convention).
+    provided for convenience, is the opposite of :py:meth:`_is_future` by
+    implementation (so same time means past, by convention).
     """
 
     return not _is_future(time, time_ref)
